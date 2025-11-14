5d978136c27e6203146ab234b18589535472d16f
Duncan Doyle <Duncan.Doyle@gmail.com>
2025-11-14 17:00:37 +0100

    Upgraded to GG 1.19. Still need to do the setup to 1.20.

--- install/env.sh
+++ install/env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export K8S_GATEWAY_API_VERSION=v1.1.0
+# export K8S_GATEWAY_API_VERSION=v1.1.0
+export K8S_GATEWAY_API_VERSION=v1.2.1
 
-export GLOO_GATEWAY_VERSION="1.18.8"
+export GLOO_GATEWAY_VERSION="1.19.8"
+# export GLOO_GATEWAY_VERSION="1.18.8"
 # export GLOO_GATEWAY_VERSION="1.17.5"
 
 export DEV_VERSION=false
@@ -11,7 +13,8 @@ export GLOO_GATEWAY_HELM_VALUES_FILE="gloo-gateway-helm-values.yaml"
 export GATEWAY_NAMESPACE=gloo-system
 
 CLUSTER_NAME="gg-demo-single"
-GLOO_PLATFORM_VERSION="2.7.0-rc2"
+# GLOO_PLATFORM_VERSION="2.7.0-rc2"
+GLOO_PLATFORM_VERSION="2.10.1"
 GLOO_PLATFORM_HELM_VALUES_FILE="gloo-platform-helm-values.yaml"
 
 # export GATEWAY_HOST=api.example.com
