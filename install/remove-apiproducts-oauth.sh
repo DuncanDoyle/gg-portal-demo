5b973ab5c7cfb316180cce9857d912a607aa33c1
Duncan Doyle <Duncan.Doyle@gmail.com>
2025-11-21 15:49:15 +0100

    Was removing incorrect authconfig and policies.

--- install/remove-apiproducts-oauth.sh
+++ install/remove-apiproducts-oauth.sh
@@ -4,13 +4,9 @@ pushd ..
 
 #----------------------------------------- ApiProducts - Remove OAuth -----------------------------------------
 
-# Revert the route to one without RouteOptions
-kubectl apply -f routes/api-example-com-root-httproute.yaml
-
 kubectl delete -f policies/routeoptions/routeoption-apiproducts-oauth.yaml
 kubectl delete -f policies/ratelimitconfigs/apiproducts-dynamic-rl.yaml
-kubectl delete -f policies/authconfigs/apiproducts-atv-opa-authconfig.yaml
-kubectl delete -f policies/opa/portal-opa-cm.yaml
+kubectl delete -f policies/authconfigs/apiproducts-atv-portalauth-authconfig.yaml
 
 popd
 
